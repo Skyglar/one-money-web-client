296b2350d1a87def5296e082974744997d973073
Skyglar <olegs.logos@gmail.com>
2022-01-18 22:50:10 +0200

    Add Antd, scss, and basic stracture

---
+++ src/components/AntComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, DatePicker, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+
+export default function AntComponent() {
+  return (
+    <div className="App">
+      <h1>antd version: {version}</h1>
+      <DatePicker />
+      <Button type="primary" style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
+    </div>
+  );
+}
