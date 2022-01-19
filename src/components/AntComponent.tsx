23e64ce66004ebb30a5bc8a54ea5fabc4a1e8496
Skyglar <olegs.logos@gmail.com>
2022-01-20 01:32:28 +0200

    succeeded test request

--- src/components/AntComponent.tsx
+++ src/components/AntComponent.tsx
@@ -1,11 +1,24 @@
 import { Button, DatePicker, version } from "antd";
+import { useEffect, useState } from "react";
 import "../assets/styles/antd.css";
 import "../assets/styles/index.css";
+import * as ValueApi from "../web.api/value.api";
 
 export default function AntComponent() {
+  const [valueState, setValueState] = useState({response: undefined});
+  debugger;
+
+  useEffect(() => {
+    debugger;
+    ValueApi.GetOrganizationClientByIdEpic("hello from client", "")
+    .then((data) => setValueState(data));
+  }, []);
+  
+  console.log(valueState);
+  debugger;
   return (
     <div className="App">
-      <h1>antd version: {version}</h1>
+      <h1>antd version: {version} {valueState.response}</h1>
       <DatePicker />
       <Button type="primary" style={{ marginLeft: 8}}>
         Primary Button
--- src/web.api/value.api.ts
+++ src/web.api/value.api.ts
@@ -1,4 +1,14 @@
-
+import { ApiUrlBuilder } from "../helpers/ApiUrlBuilder"
+import * as api from "../web.api/api.constants";
 
 export const GetOrganizationClientByIdEpic = (action$: any, store: any) =>
-{}
+{
+    debugger;
+    let query = ApiUrlBuilder.Build(api.TEST_API_TRIGGER, "uk/") + action$;
+    
+    return fetch(query, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+      })
+    .then((response) => response.json())
+}
