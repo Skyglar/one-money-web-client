19d346c5a3f6e174484064dd7ccd09816ea98110
Artem Tuchonuk <irascible.dog@gmail.com>
2022-01-19 13:54:21 +0200

    libraries fixed

--- src/components/AntComponent.tsx
+++ src/components/AntComponent.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import { Button, DatePicker, version } from "antd";
-import "antd/dist/antd.css";
-import "./index.css";
+import "../assets/styles/antd.css";
+import "../assets/styles/index.css";
 
 export default function AntComponent() {
   return (
     <div className="App">
       <h1>antd version: {version}</h1>
       <DatePicker />
-      <Button type="primary" style={{ marginLeft: 8 }}>
+      <Button type="primary" style={{ marginLeft: 8, colorScheme: "#13c2c2"}}>
         Primary Button
       </Button>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import AntComponent from './components/AntComponent'
+import AntComponent from './components/AntComponent';
+import "antd/dist/antd.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
